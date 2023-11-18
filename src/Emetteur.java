417d4f324171747009d180f418f752703851e3ef
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2023-11-18 18:33:05 -0500

    création de récepteur et Emetteur
    Création de Emetteur et Recepteur, vu que ce ne sont pas des objet elles n'hérite de rien, elles auront des fonctions similaires mais différentes donc pas besoin de classe abstraite
    Les comms mentionnent l'énoncé de ce qui est attendu.

---
+++ src/Emetteur.java
@@ -0,0 +1,9 @@
+public class Emetteur {
+	
+	/* Ce que cette classe doit faire :
+	 * • Lire des données de fichier,
+	 * • Produire des trames et les envoyer,
+	 * • Attendre et traiter les accusés de réception,
+	 * • Ré-envoyer les données en cas de perte ou d’erreur.
+	 */
+}
---
+++ src/Recepteur.java
@@ -0,0 +1,8 @@
+public class Recepteur {
+	/* Ce que cette classe doit faire :
+	 * • Recevoir des trames
+	 * • Vérifier la présence d’erreurs ou non dans les trames reçues
+	 * • Envoyer des accusés de réception (RR).
+	 * • Envoyer des REJ en cas d’erreur.
+	 */
+}

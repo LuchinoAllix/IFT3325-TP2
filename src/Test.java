a37bce6f9b81f29bbe3d8827226288f7b4acd060
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2023-11-18 18:28:02 -0500

    Création Test.java
    Pour le moment ne sert à rien (hello world) mais sera utilisée pour debug et lancer les test

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+class Test{
+	/**
+	 * Classe pour implémenter les différentes façon de tester le protocol conçu. 
+	*/
+
+	public static void main(String[] args){
+	System.out.println("Hello world");
+	}
+
+}
+

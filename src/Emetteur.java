b2bf0e07b22e0ed866f0468febd93f94f737da68
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2023-11-25 14:56:08 -0500

    Implémentation sockets
    Utilisation des sockets dans Emetteur et Recepteur

--- src/Emetteur.java
+++ src/Emetteur.java
@@ -1,9 +1,46 @@
-public class Emetteur {
+import java.net.*;
+import java.io.*;
+
+public class Emetteur{
 	
 	/* Ce que cette classe doit faire :
 	 * • Lire des données de fichier,
 	 * • Produire des trames et les envoyer,
 	 * • Attendre et traiter les accusés de réception,
 	 * • Ré-envoyer les données en cas de perte ou d’erreur.
-	 */
+	*/
+
+	private Socket clientSocket;
+	private PrintWriter out;
+	private BufferedReader in;
+
+
+	public void startConnection(String ip, int port) throws UnknownHostException, IOException {
+		clientSocket = new Socket(ip, port);
+		out = new PrintWriter(clientSocket.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	}
+
+	public String sendMessage(String msg) throws IOException {
+		out.println(msg);
+		String resp = in.readLine();
+		return resp;
+	}
+
+	public void stopConnection() throws IOException {
+		in.close();
+		out.close();
+		clientSocket.close();
+	}
+
+	public static void main(String[] args) {
+		Emetteur client = new Emetteur();
+		try {
+			client.startConnection("127.0.0.1", 6669);
+			String response = client.sendMessage("hello server");
+			System.out.println(response);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/Recepteur.java
+++ src/Recepteur.java
@@ -1,8 +1,51 @@
-public class Recepteur {
+import java.net.*;
+import java.io.*;
+
+// Source socket : https://www.baeldung.com/a-guide-to-java-sockets
+
+public class Recepteur{
 	/* Ce que cette classe doit faire :
 	 * • Recevoir des trames
 	 * • Vérifier la présence d’erreurs ou non dans les trames reçues
 	 * • Envoyer des accusés de réception (RR).
 	 * • Envoyer des REJ en cas d’erreur.
 	 */
+
+	private ServerSocket serverSocket;
+	private Socket clientSocket;
+	private PrintWriter out;
+	private BufferedReader in;
+
+	public void start(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+		clientSocket = serverSocket.accept();
+		out = new PrintWriter(clientSocket.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		String greeting = in.readLine();
+            if ("hello server".equals(greeting)) {
+                out.println("hello client");
+            }
+            else {
+                out.println("unrecognised greeting");
+            }
+	}
+
+	public void stop() throws IOException {
+		in.close();
+		out.close();
+ 
+		clientSocket.close();
+		serverSocket.close();
+	}
+
+	public static void main(String[] args) {
+		Recepteur server = new Recepteur();
+		try {
+			server.start(6669);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
 }

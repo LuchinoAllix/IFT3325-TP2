63f030579b1efdbd71ac3db27469a2bb45b3b8fd
Hayen03 <jetzer.leo@gmail.com>
2023-11-29 23:03:31 -0500

    ajout d'un drain à Receiver

--- src/Receiver.java
+++ src/Receiver.java
@@ -43,23 +43,28 @@ public class Receiver{
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		OutputStream out = new VoidStream();
+		byte[] poubelle = new byte[512]; // pour vider le buffer
 		int port=0;
 		if(args.length !=1){System.out.println("Il faut 1 argument. (<port>)");System.exit(0);}
 		try {
 			port = Integer.parseInt(args[0]);
 			start(port);
-			byte[] poubelle = new byte[512]; // pour vider le buffer
 			while (!io.estFerme()) {
 				try {Thread.sleep(100);} catch (InterruptedException e) {}
-				if (io.estConnecte()) io.getInputStream().read(poubelle);
+				if (io.estConnecte()) out.write(io.getInputStream().read(poubelle));
 			}
-			stop();
 		} catch (NumberFormatException e) {
 			System.out.println("Le numéro de port doit être un entier.");
 			System.exit(0);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(0);
+		} finally {
+			try {
+				stop();
+				out.close();
+			} catch (IOException e) {}
 		}
 	}
 }
